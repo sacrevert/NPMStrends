4c2001509283d409765de22377236255ee14f73a
sacrevert <olipes@ceh.ac.uk>
2018-08-21 13:38:45 +0100

    first commit

---
+++ scripts/1_getDataFromIndiciaFuns.R
@@ -0,0 +1,82 @@
+## 1. Functions to retrieve plot/sample/habitat/level data from Indicia/NPMS database (to avoid hardcoding passwords in script)
+# O.L. Pescott
+# 17.08.2018
+
+# SQL query is also in W:\PYWELL_SHARED\Pywell Projects\BRC\_BRC_projects\NPMS\Analyses\2018 08 - Per species trend analyses\SQL\extractData_v0.0.sql
+
+####
+#### Warning! This is slow (11 mins if retrieving data up between 2015-mid-2018) and will get increasingly slower
+#### Need to investigate possible speed-ups/new cache table that runs this automatically on some periodic basis
+####
+
+############# Function to retrieve plot and sample level attributes
+## Change dates as appropriate! (Lines 23 and 36)
+getNpmsData_PlotsSamples <- function(dsn = "PostgreSQL35W", user, password) {   channel <- RODBC::odbcConnect(dsn = dsn, uid = user, pwd = password)
+                                                                    temp <- RODBC::sqlQuery(channel, paste(
+                                                                    "select sq.plot_id, sq.monad, occs.sample, occs.title, habs.term as surv_habitat",
+                                                                    "from",
+	                                                                    "(select s.id as sample, su.title",
+                                                                      "from samples s",
+                                                                      "left join cache_occurrences co",
+                                                                      "on co.sample_id = s.id",
+                                                                      "left join surveys su",
+                                                                      "on su.id = s.survey_id",
+                                                                      "where s.date_start between '2015-01-01' and '2018-08-15'",
+                                                                      "and s.survey_id in (87,154,155)",
+                                                                      "group by s.id, su.title",
+                                                                      ") occs",
+                                                                    "join",
+                                                                      "(select s.id sample_id, t.term",
+                                                                      "from samples s, sample_attributes sa, sample_attribute_values sav, termlists_terms tt, terms t",
+                                                                      "where s.id = sav.sample_id",
+                                                                      "and sav.sample_attribute_id = sa.id",
+                                                                      "and tt.termlist_id = sa.termlist_id",
+                                                                      "and tt.id = sav.int_value",
+                                                                      "and t.id = tt.term_id",
+                                                                      "and s.survey_id in (87,154,155)",
+                                                                      "and s.date_start between '2015-01-01' and '2018-08-15'",
+                                                                      "and int_value is not null",
+                                                                      "and sa.termlist_id is not null",
+                                                                      "and sa.caption = 'NPMS Habitat'",
+                                                                      "order by sample_attribute_id, sample_id",
+                                                                      ") habs",
+                                                                    "on occs.sample = habs.sample_id",
+                                                                    "join",
+                                                                      "(select distinct square.centroid_sref as monad, plot.id as plot_id, visit.id as sample_id",
+                                                                      "from samples visit",
+                                                                      "join locations plot on plot.id = visit.location_id AND plot.deleted=false",
+                                                                      "join locations square on square.id = plot.parent_id AND square.deleted=false",
+                                                                      "where visit.survey_id in (87,154,155) AND visit.deleted=false",
+                                                                      "group by square.centroid_sref, plot.id, visit.id",
+                                                                      ") sq",
+                                                                    "on occs.sample = sq.sample_id"))
+                                                                    RODBC::odbcClose(channel)
+                                                                    rm(channel)
+                                                                    return(temp)
+}
+
+############# Function to retrieve taxon data for samples
+## Change dates as appropriate! (Lines 67 and 76)
+getNpmsData_SamplesSpecies <- function(dsn = "PostgreSQL35W", user, password) {   channel <- RODBC::odbcConnect(dsn = dsn, uid = user, pwd = password)
+                                                                                  temp <- RODBC::sqlQuery(channel, paste( ## Wildflower data
+                                                                                    "select co.id, co.sample_id, co.date_start as date, co.preferred_taxon, co.taxa_taxon_list_external_key as tvk,  oav.int_value as domin, co.record_status as record_status, co.sensitivity_precision",
+                                                                                    "from indicia.cache_occurrences co",
+                                                                                    "LEFT join indicia.occurrence_attribute_values oav on oav.occurrence_id = co.id and oav.deleted=false",
+                                                                                    "LEFT join indicia.occurrence_attributes oa on oa.id=oav.occurrence_attribute_id",
+                                                                                    "AND oa.id = 104 and oav.deleted=false Where co.survey_id in (87) and co.training = 'f'",
+                                                                                    "and co.date_start between '2015-01-01' and '2018-08-15'",
+                                                                                    "group by co.id, co.sample_id, co.date_start, co.preferred_taxon, co.taxa_taxon_list_external_key, oav.int_value, co.record_status, co.sensitivity_precision;"))
+                                                                                  temp2 <- RODBC::sqlQuery(channel, paste( ## Indicator and Inventory data
+                                                                                    "select co.id, co.sample_id, co.date_start as date, co.preferred_taxon, co.taxa_taxon_list_external_key as tvk,  terms.term as domin, co.record_status as record_status, co.sensitivity_precision",
+                                                                                    "from indicia.cache_occurrences co",
+                                                                                    "LEFT join indicia.occurrence_attribute_values oav on oav.occurrence_id = co.id  and oav.deleted=false",
+                                                                                    "LEFT join indicia.occurrence_attributes oa on oa.id=oav.occurrence_attribute_id AND oa.id = 214 and oav.deleted=false",
+                                                                                    "LEFT join indicia.termlists_terms tt on tt.id=oav.int_value and tt.deleted=false",
+                                                                                    "LEFT join indicia.terms terms on terms.id = tt.term_id AND terms.deleted=false Where co.survey_id in (154,155) and co.training = 'f'",
+                                                                                    "and co.date_start between '2015-01-01' and '2018-08-15'",
+                                                                                    "group by co.id, co.sample_id, co.date_start, co.preferred_taxon, co.taxa_taxon_list_external_key, terms.term, co.record_status, co.sensitivity_precision;"))
+                                                                                  temp3 <- rbind(temp, temp2[-1,]) ## rbind but remove header from second file
+                                                                                  RODBC::odbcClose(channel)
+                                                                                  rm(channel)
+                                                                                  return(temp3)
+}
---
+++ scripts/3_processData.R
@@ -0,0 +1,104 @@
+## 3. Process retrieved data (plot/sample level info and species per sample info)
+## in order to create per species information for modelling
+# O.L. Pescott
+# 17.08.2018
+rm(list=ls())
+
+## Load data (previously extracted using functions in script 1)
+load(file = "data/npms1518_PlotsSamples_17Aug2018.Rdata") # plot/sample data
+load(file = "data/npms1518_SamplesSpecies_17Aug2018.Rdata") # sample/species data
+#
+
+## Read in the official list of indicators with indicia preferred names and TVKs
+# file has one row for every species::fine-scale habitat association
+# broad habitat lists can be derived from another column indicating fine::broad habitat associations
+inds <- read.csv(file = "data/npmsIndicatorsIndicia_Aug2018.csv", header = T, stringsAsFactors = F)
+## NOTE THAT THIS FILE MIGHT NEED TO BE OCCASIONALLY UPDATED SO THAT IT IS IN LINE WITH THE PREFERRED TAXON ACCORDING TO THE TAXA TABLE IN INDICIA! ##
+# UNFORTUNATELY THIS IS CURRENTLY A MANUAL TASK, BECAUSE THE HABITAT:SPECIES INFORMATION FOR WILDFLOWER/INDICATOR SPECIES IS NOT CONTAINED IN THE DATABASE ##
+# working is here: W:\PYWELL_SHARED\Pywell Projects\BRC\_BRC_projects\NPMS\Technical work\Species
+# last file: NPMS_FINAL_CONCISE_withBRC_IndiciaUpdateAug2018.xslx
+#
+
+## Process indicator data for later function
+head(inds)
+indsF <- aggregate(guidanceSpecies ~ indiciaName + indiciaPrefTvk + fine.scale_habitat + indicator + indicator_type + wildflower,
+                   data = inds, function(x) length(x))
+indsB <- aggregate(guidanceSpecies ~ indiciaName + indiciaPrefTvk + broad.scale_habitat + indicator + indicator_type + wildflower,
+                   data = inds, function(x) length(x))
+indsF <- indsF[,-(7)]; indsB <- indsB[,-(7)]; # delete the unnecessary aggregated columns
+
+## These steps are all about making a lookup table that can later tell us, for every posisble indicator/wildflower v. broad/fine habitat combination,
+# whether species X is on the list for that combination
+indsF$combinedI <- ifelse(indsF$indicator == "y", paste(indsF$fine.scale_habitat,", Indicator survey", sep = ""), NA)
+indsF$combinedW <- ifelse(indsF$wildflower == "y", paste(indsF$fine.scale_habitat,", Wildflower survey", sep = ""), NA)
+indsB$combinedI <- ifelse(indsB$indicator == "y", paste(indsB$broad.scale_habitat,", Indicator survey", sep = ""), NA)
+indsB$combinedW <- ifelse(indsB$wildflower == "y", paste(indsB$broad.scale_habitat,", Wildflower survey", sep = ""), NA)
+
+# for fine-scale habs
+indsF_tmp1 <- indsF[,c("indiciaName", "indiciaPrefTvk", "combinedI")]
+indsF_tmp2 <- na.omit(indsF[,c("indiciaName", "indiciaPrefTvk", "combinedW")])
+names(indsF_tmp1)[3] <- "combined"; names(indsF_tmp2)[3] <- "combined"
+indsF_minimal <- rbind(indsF_tmp1, indsF_tmp2)
+
+# for broad-scale habs
+indsB_tmp1 <- indsB[,c("indiciaName", "indiciaPrefTvk", "combinedI")]
+indsB_tmp2 <- na.omit(indsB[,c("indiciaName", "indiciaPrefTvk", "combinedW")])
+names(indsB_tmp1)[3] <- "combined"; names(indsB_tmp2)[3] <- "combined"
+indsB_minimal <- rbind(indsB_tmp1, indsB_tmp2)
+
+indsLookup <- rbind(indsF_minimal, indsB_minimal) # combine
+
+## function to select the relevant samples for any given set of habitats
+getSamples <- function(habsList){ temp <- merge(npms15_18spp, npms15_18, by.x = 'sample_id', by.y = "sample", all.x = T, all.y = F)
+                                  temp2 <- temp[temp$surv_habitat %in% habsList,]
+                                  return(temp2)
+                                }
+# END function
+
+## Grasslands examples
+grasslands <- c("Neutral pastures and meadows", "Dry acid grassland", "Dry calcareous grassland", "Neutral damp grassland", "Lowland grassland")
+grassSamples <- getSamples(habsList = grasslands)
+#
+
+## Then process the filtered data so that species abundance information is as it should be (i.e. present/absent/NA)
+# try Achillea millefolium first (outside of function, in order to develop principle)
+#spSamplePA <- function(samples, species){
+#                                         temp <- aggregate(preferred_taxon ~ sample_id, data = grassSamples, function(x) max(ifelse(x == "Achillea millefolium", 1, 0)))
+#                                         presence <- temp[temp$preferred_taxon == 1,] # samples containing the taxon of interest
+#                                         presSamps <- merge(presence, npms15_18, by.x = "sample_id", by.y = "sample", all.x = T, all.y = F) # useful for subsequent rbind
+#                                         presSamps$combination <- paste(presSamps$surv_habitat,", ",presSamps$title, sep = "") # useful for subsequent rbind
+#                                         absence <- temp[temp$preferred_taxon == 0,] # samples NOT containing the taxon of interest (but are these 0 or NA?)
+#                                         absSamps <- merge(absence, npms15_18, by.x = "sample_id", by.y = "sample", all.x = T, all.y = F) # habitat/level info for every sample
+#                                         absSamps$combination <- paste(absSamps$surv_habitat,", ",absSamps$title, sep = "") # create column for lookup to indsLookup
+#                                         indsLookup_fil <- indsLookup[indsLookup$indiciaName == "Achillea millefolium",]
+#                                         absSamps_AN <- merge(absSamps, indsLookup_fil, by.x = "combination", by.y = "combined", all.x = T, all.y = F)# absent or NA indicator
+#                                         presSamps_AN <- merge(presSamps, indsLookup_fil, by.x = "combination", by.y = "combined", all.x = T, all.y = F)# again, for rbind
+#                                         absSamps_AN$preferred_taxon <- ifelse(is.na(absSamps_AN$indiciaName), "NA", 0)
+#                                         # but inventory samples should always be zero if absent
+#                                         absSamps_AN$preferred_taxon <- ifelse(absSamps_AN$title =="Inventory survey", 0, absSamps_AN$preferred_taxon)
+#                                         samples_PAN <- rbind(presSamps_AN, absSamps_AN) # combine so we have a PAN (pres, abs, NA) view on our focal species
+                                        # return(samples_PAN)
+                                        # }
+
+###########################################
+## Generalise to make the function function
+###########################################
+spSamplePA <- function(samples, species){ temp <- aggregate(preferred_taxon ~ sample_id, data = samples, function(x) max(ifelse(x == species, 1, 0)))
+                                          presence <- temp[temp$preferred_taxon == 1,] # samples containing the taxon of interest
+                                          presSamps <- merge(presence, npms15_18, by.x = "sample_id", by.y = "sample", all.x = T, all.y = F) # useful for subsequent rbind
+                                          presSamps$combination <- paste(presSamps$surv_habitat,", ",presSamps$title, sep = "") # useful for subsequent rbind
+                                          absence <- temp[temp$preferred_taxon == 0,] # samples NOT containing the taxon of interest (but are these 0 or NA?)
+                                          absSamps <- merge(absence, npms15_18, by.x = "sample_id", by.y = "sample", all.x = T, all.y = F) # habitat/level info for every sample
+                                          absSamps$combination <- paste(absSamps$surv_habitat,", ",absSamps$title, sep = "") # create column for lookup to indsLookup
+                                          indsLookup_fil <- indsLookup[indsLookup$indiciaName == species,]
+                                          absSamps_AN <- merge(absSamps, indsLookup_fil, by.x = "combination", by.y = "combined", all.x = T, all.y = F)# absent or NA indicator
+                                          presSamps_AN <- merge(presSamps, indsLookup_fil, by.x = "combination", by.y = "combined", all.x = T, all.y = F)# again, for rbind
+                                          absSamps_AN$preferred_taxon <- ifelse(is.na(absSamps_AN$indiciaName), "NA", 0)
+                                          # but inventory samples should always be zero if absent
+                                          absSamps_AN$preferred_taxon <- ifelse(absSamps_AN$title =="Inventory survey", 0, absSamps_AN$preferred_taxon)
+                                          samples_PAN <- rbind(presSamps_AN, absSamps_AN) # combine so we have a PAN (pres, abs, NA) view on our focal species
+                                          return(samples_PAN)
+}
+
+## Let's see if it works!
+Achi_mill_PAN <- spSamplePA(samples = grassSamples, species = "Achillea millefolium") # should give 2003 obs of 9 vars -- tick!
