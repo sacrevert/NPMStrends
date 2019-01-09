aad6326d32115535ab7020ed81f70427ba8a2ac5
Pescott <olipes@ceh.ac.uk>
2019-01-09 17:09:07 +0000

    rodbc error correction

--- scripts/1_getDataFromIndiciaFuns.R
+++ scripts/1_getDataFromIndiciaFuns.R
@@ -12,7 +12,7 @@
 ############# Function to retrieve plot and sample level attributes
 ## Change dates as appropriate! (Lines 23 and 36)
 getNpmsData_PlotsSamples <- function(dsn = "PostgreSQL30", user = "brc_read_only", password) {   
-                                                                    channel <- RODBC::odbcConnect(dsn = dsn, uid = user, password = password)
+                                                                    channel <- RODBC::odbcConnect(dsn = dsn, uid = user, pwd = password)
                                                                     temp <- RODBC::sqlQuery(channel, paste(
                                                                     "set search_path to indicia;",
                                                                     "select sq.plot_id, sq.monad, occs.sample, occs.title, habs.term as surv_habitat",
@@ -60,7 +60,7 @@ getNpmsData_PlotsSamples <- function(dsn = "PostgreSQL30", user = "brc_read_only
 ############# Function to retrieve taxon data for samples
 ## Change dates as appropriate! (Lines 67 and 76)
 getNpmsData_SamplesSpecies <- function(dsn = "PostgreSQL30", user = "brc_read_only", password) {   
-                                                                                  channel <- RODBC::odbcConnect(dsn = dsn, uid = user, password = password)
+                                                                                  channel <- RODBC::odbcConnect(dsn = dsn, uid = user, pwd = password)
                                                                                   temp <- RODBC::sqlQuery(channel, paste( ## Wildflower data
                                                                                     "select co.id, co.sample_id, co.date_start as date, co.preferred_taxon, co.taxa_taxon_list_external_key as tvk,  oav.int_value as domin, co.record_status as record_status, co.sensitivity_precision",
                                                                                     "from indicia.cache_occurrences co",
---
+++ scripts/2_getDataFromIndiciaExec.R
@@ -0,0 +1,39 @@
+## 2. Execute function to retrieve plot/sample/habitat/level data from Indicia/NPMS database (to avoid hardcoding passwords in script)
+# O.L. Pescott
+# 17.08.2018
+
+# SQL query is also in W:\PYWELL_SHARED\Pywell Projects\BRC\_BRC_projects\NPMS\Analyses\2018 08 - Per species trend analyses\SQL\extractData_v0.0.sql
+
+####
+#### Warning! This is slow (11 mins if retrieving data up between 2015-mid-2018) and will get increasingly slower
+#### Need to investigate possible speed-ups/new cache table that runs this automatically on some periodic basis
+####
+#rm(list=ls())
+
+## Check for RODBC etc. and install if not available
+list.of.packages <- c("RODBC", "rstudioapi")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+library(RODBC)
+##
+source(file = "scripts/1_getDataFromIndiciaFuns.R") # source SQL functions
+password <- rstudioapi::askForPassword("Database password")
+##
+
+## Get plot and sample data!
+# columns: plot_id, monad, sample, title (survey), surv_habitat
+npms15_18plots <- getNpmsData_PlotsSamples(password = password)
+# change this so that file name automatically updates with system date - 09.01.2019
+#save(npms15_18plots, file = "data/npms1518_PlotsSamples_17Aug2018.Rdata") # 10,581 rows -- old version - 17.08.2018
+save(npms15_18plots, file = paste("data/npms1518_PlotsSamples_", as.character(Sys.Date()), ".Rdata", sep = ""))
+
+## Get taxon data across samples
+npms15_18spp <- getNpmsData_SamplesSpecies(password = password)
+# change this so that file name automatically updates with system date - 09.01.2019
+#save(npms15_18spp, file = "data/npms1518_SamplesSpecies_17Aug2018.Rdata") # 101,508 -- old version - 17.08.2018
+save(npms15_18spp, file = paste("data/npms1518_SamplesSpecies_", as.character(Sys.Date()), ".Rdata", sep =""))
+
+# regarding the row count discrepancies, see the two versions of the SQL in 
+# W:\PYWELL_SHARED\Pywell Projects\BRC\_BRC_projects\NPMS\Analyses\2018 08 - Per species trend analyses\SQL\extractData_v0.0.sql
+
+## END
